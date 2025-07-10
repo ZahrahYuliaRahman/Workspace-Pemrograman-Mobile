c402d8019a6019d822fe7b9b26707190cc4c17bf
ZahrahYuliaRahman <zahrasukma12@gmail.com>
2025-07-10 15:16:20 +0700

    Menambahkan Project 4

---
+++ rah_shopping_list/android/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "id.ac.polnep.rah_shopping_list"
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "id.ac.polnep.rah_shopping_list"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ rah_shopping_list/android/app/src/main/kotlin/id/ac/polnep/rah_shopping_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.ac.polnep.rah_shopping_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
